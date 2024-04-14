96858912445568234212c33fe1d894c20f971df3
AliMarshmallow <79265188+AliMarshmallow@users.noreply.github.com>
2024-04-14 18:24:19 +0330

    add telegram message sender

---
+++ src/edu/sharif/selab/services/MessageService.java
@@ -0,0 +1,13 @@
+package edu.sharif.selab.services;
+
+import edu.sharif.selab.models.EmailMessage;
+import edu.sharif.selab.models.SmsMessage;
+import edu.sharif.selab.models.TelegramMessage;
+
+public interface MessageService {
+    public void sendSmsMessage(SmsMessage smsMessage);
+    public void sendEmailMessage(EmailMessage emailMessage);
+
+    public void sendTelegramMessage(TelegramMessage telegramMessage);
+
+}
